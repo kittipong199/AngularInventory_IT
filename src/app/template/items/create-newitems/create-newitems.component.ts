1a9bd75bdce6ee3cd1f95ba99cffbb3e84431d70
kittipong199 <68522978+kittipong199@users.noreply.github.com>
2022-11-17 15:19:00 +0700

    Update create-newitems.component.ts

--- src/app/template/items/create-newitems/create-newitems.component.ts
+++ src/app/template/items/create-newitems/create-newitems.component.ts
@@ -1,4 +1,16 @@
+
 import { Component, OnInit } from '@angular/core';
+
+// service
+import { ItemsService } from 'src/app/service/items.service';
+import { ProductsService } from 'src/app/service/products.service';
+//model
+import { Product } from 'src/app/models/product';
+import { ItemAll,postItem } from 'src/app/models/items.model';
+
+
+import {NgForm,FormGroup, FormControl, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-create-newitems',
@@ -7,9 +19,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateNewitemsComponent implements OnInit {
 
-  constructor() { }
+// [x: string]: FormGroup<any>;
+
+  // addItem: any = {};
+
+  constructor(private ps:ProductsService) { }
+
+   ItemAllList: ItemAll[]= [];
+  // addItem: postItem[] = [];
+
 
   ngOnInit(): void {
-  }
 
-}
+
+   }
+
+  //  itemForm = new FormGroup({
+  //   'serial_no':new FormControl('',Validators.required),
+  //   'brand_name':new FormControl('',Validators.required),
+  //   'type_name':new FormControl('',Validators.required),
+  //   'fixasset':new FormControl('',Validators.required)
+  //  });
+
+  //  itemSubmit(){
+  //     console.log(this.itemForm.value);
+
+  //  }
+
+   addNewProduct(product: any){
+
+
+    console.log(this.productNewData.value);
+    this.ps.addProduct(product);
+
+  }
+  //  onSubmit(itemForm: NgForm){
+  //   // ดึง data value from ngModule in html
+
+  //   // const values = itemForm.value;
+  //   console.log(itemForm.value);
+  //   // alert(value);
+
+  //  }
+
+   // get all data from item table
+    // getData(){
+    //  this.ApiService.getAllItem().subscribe((data: ItemAll[])=> {
+    //    console.log(data, "res ==>");
+    //    this.ItemAllList = data;
+    //  });
+
+    // }
+
+  //  createNewItem(){
+  //   this.Newitemform = this.
+  //  }
+
+
+  }
